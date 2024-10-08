b02d85cd10355434c6d987b3d35073f65b91f09e
OnurK <onurcankarateke1@gmail.com>
2024-10-08 14:07:42 +0300

    Entity&DB Layer Added

---
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Concrete
+{
+   public class Context : IdentityDbContext<AppUser, AppRole, int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ONUR\\SQLEXPRESS;initial catalog=EasyCashDB;user Id=sa;password=12345;Trusted_Connection=true;");
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountsProcesses { get; set; }
+        
+    
+    }
+}
