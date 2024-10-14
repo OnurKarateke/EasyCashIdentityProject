0d632ce510d9e3e99a30e23c343bb8632a5ac7dd
OnurK <onurcankarateke1@gmail.com>
2024-10-14 20:43:04 +0300

    Mail Confirm Code

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -1,7 +1,11 @@
 ﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
 using EasyCashIdentityProject.EntityLayer.Concrete;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using System.Net.Mail;
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
@@ -26,18 +30,43 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
             if (ModelState.IsValid)
             {
                 Random random = new Random();
+                int code = random.Next(100000, 1000000);
+
                 AppUser appUser = new AppUser()
                 {
                     UserName = appUserRegisterDto.Username,
                     Name = appUserRegisterDto.Name,
                     Surname = appUserRegisterDto.Surname,
                     Email = appUserRegisterDto.EmailAdrress,
-                    ConfirmCode = random.Next(100000, 1000000)
-                    
+                    City = "Antalya",
+                    Disctrict = "Kepez",
+                    ImageUrl = "cccc",
+                    ConfirmCode = code
+
                 };
                 var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
                 if (result.Succeeded)
                 {
+                    MimeMessage mimeMessage = new MimeMessage();
+                    MailboxAddress mailboxAddressFrom = new MailboxAddress("Easy Cash Admin", "onurcankarateke2@gmail.com");
+                    MailboxAddress mailboxAdressTo = new MailboxAddress("User", appUser.Email);
+
+                    mimeMessage.From.Add(mailboxAddressFrom);
+                    mimeMessage.To.Add(mailboxAdressTo);
+
+                    var bodybuilder = new BodyBuilder();
+                    bodybuilder.TextBody = "Your code for registration: " + code;
+                    mimeMessage.Body = bodybuilder.ToMessageBody();
+
+                    mimeMessage.Subject = "Easy Cash Confirmation Code"; 
+
+                    SmtpClient client = new SmtpClient();
+                    client.Connect("smtp.gmail.com", 587, false);
+                    client.Authenticate("onurcankarateke2@gmail.com", "thlskfglzyycibbq");
+                    client.Send(mimeMessage);
+                    client.Disconnect(true);
+                   
+
                     return RedirectToAction("Index", "ConfirmMail");
                 }
                 else
